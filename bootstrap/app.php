7d1b25b76b5aae898f23c10614893cf1684b1fa1
programmercito <osboceo@live.com>
2025-09-02 16:09:36 -0400

    fix the login error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Illuminate\Auth\AuthenticationException $e, Illuminate\Http\Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'No autenticado.'
+                ], 401);
+            }
+        });
     })
     ->create();
